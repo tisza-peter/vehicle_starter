5ee755589c3804dd9ee9e6572f66258f95df652a
tiszapeter <tiszapeter82@gmail.com>
2023-01-21 22:17:29 +0100

    init

--- src/main/java/com/example/vehicle_starter/VehicleStarterApplication.java
+++ src/main/java/com/example/vehicle_starter/VehicleStarterApplication.java
@@ -1,13 +1,20 @@
 package com.example.vehicle_starter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.vehicle_core.Store_Repository_Interface;
+import com.example.vehicle_core.VehicleCoreApplication;
+import com.example.vehicle_cui.UI_Controller;
+import com.example.vehicle_cui.UI_Presenter_Implementation;
+import com.example.vehicle_store.Store_Repository_Implementation;
 
-@SpringBootApplication
 public class VehicleStarterApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(VehicleStarterApplication.class, args);
+        Store_Repository_Interface vehicleRepository = new Store_Repository_Implementation();
+        UI_Presenter_Implementation vehiclePresenter = new UI_Presenter_Implementation();
+        UI_Controller vehicleUIController = new UI_Controller();
+        VehicleCoreApplication vehicleCore = new VehicleCoreApplication();
+        vehicleCore.SetStore(vehicleRepository);
+        vehicleCore.SetUI(vehiclePresenter);
+        vehicleUIController.SetCore(vehicleCore);
+        vehicleUIController.Start();
     }
-
 }
--- src/test/java/com/example/vehicle_starter/VehicleStarterApplicationTests.java
+++ src/test/java/com/example/vehicle_starter/VehicleStarterApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.vehicle_starter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class VehicleStarterApplicationTests {
 
     @Test
