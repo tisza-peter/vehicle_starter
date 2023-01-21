0143ab48d1f1322f8c604281cb5d1721421eb7bf
tiszapeter <tiszapeter82@gmail.com>
2023-01-21 22:17:10 +0100

    Initial commit

---
+++ src/main/java/com/example/vehicle_starter/VehicleStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vehicle_starter/VehicleStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicle_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
